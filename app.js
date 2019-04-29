2c8fb56a7f7bf0366c7748e4bbe65d505df366f0
Parado <timeandparadox@gmail.com>
2019-04-28 21:04:17 -0400

    Total rewrite. Added Menu Screen, fixed all known bugs, added leaderboard.

--- app.js
+++ app.js
@@ -3,122 +3,209 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const path = require('path');
-let game = 0;
-let dropped = {};
-const timeout = 100;
+let game = null;
+const colors = [[255, 255, 255], [255, 0, 255], [0, 0, 255], [0, 255, 255], [255, 255, 0], [196, 0, 60]];
+const UP = 0, RIGHT = 1, DOWN = 2, LEFT = 3;
+const lengthIncrease = 3;
 
-app.use(express.static(__dirname + '/public'));
-
+app.use(express.static(__dirname + "/public"));
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/snake.html');
 });
 
-
 io.on('connection', function(socket){
-    dropped[socket.id] = 0;
-    let dead = false;
-    console.log("user connected " + socket.id);
-    if(game == 0){
-        game = new Game();
-    }
-    game.players[socket.id] = new Player();
-    socket.emit('connectdata', game.players[socket.id].data);
-    socket.on('updatereply', function(_data){
-        if(!dead){
-            game.players[socket.id].updatePieces(_data["pieces"]);
-            dropped[socket.id] = 0;
+    socket.on('newplayer', function(data){
+        if(game == null){
+            game = new Game();
+        }
+        game.players.push(new Player(data["name"], socket.id));
+    });
+    socket.on('changeDir', function(data){
+        if(game != null){
+            for(let i = 0; i < game.players.length; i++){
+                if(game.players[i].id == socket.id){
+                    game.players[i].changeDir(data["dir"]);
+                }
+            }
         }
     });
-    socket.on('spawnapple', function(_data){
-        game.spawnApple();
-    });
-    socket.on('die', function(_data){
-        if(game != 0){
-            delete game.players[socket.id];
-            dead = true;
-        }
-    });
-    socket.on('disconnect', function(_data){
-        if(game != 0){
-            delete game.players[socket.id];
-            dead = true;
+    socket.on('disconnect', function(data){
+        for(let i = 0; game != null && i < game.players.length; i++){
+            if(game.players[i].id == socket.id){
+                game.players[i].die();
+            }
         }
     });
 });
-
 
 http.listen(process.env.PORT || 3000, function(){
-    console.log('listening');
+    console.log("Hello World!");
 });
 
-
 class Player {
-    constructor(){
-        let color1 = Math.floor(Math.random() * 255);
-        let color2, color3;
-        if(color1 < 128){
-            color2 = Math.floor(Math.random() * 127) + 128;
-        }
-        else{
-            color2 = Math.floor(Math.random() * 127);
-        }
-        if(color1 + color2 < 255){
-            color3 = Math.floor(Math.random() * 127) + 128;
-        }
-        else{
-            color3 = Math.floor(Math.random() * 127);
-        }
-        this.color = [color1, color2, color3];
-        this.pos = [Math.floor(Math.random() * 64), Math.floor(Math.random() * 48)];
-        this.pieces = [];
-        this.pieces.push([this.pos[0], this.pos[1]]);
-    }
-    get data(){
-        return {
-            "color": this.color,
-            "pos": this.pos,
-            "pieces": this.pieces
-        };
-    }
-    updatePieces(pieces){
-        this.pieces = pieces;
+    constructor(name, id){
+        this.name = name;
+        this.color = colors[Math.floor(Math.random() * colors.length)];
+        this.tail = [];
+        this.head = game.safeSpawnPos();
+        this.dir = RIGHT;
+        this.nextDir = this.dir;
+        this.length = 3;
+        this.id = id;
+        this.dead = false;
     }
 
+    get data(){
+        return {"name": this.name,
+                "color": this.color,
+                "score": this.tail.length + 1};
+    }
+
+    changeDir(newDir){
+        if(this.dir != newDir - 2 && this.dir != newDir + 2){
+            this.nextDir = newDir;
+        }
+    }
+
+    move(){
+        if(!this.dead){
+            this.dir = this.nextDir;
+            this.tail.push([this.head[0], this.head[1]]);
+            if(this.dir == RIGHT){
+                this.head[0]++;
+            } else if(this.dir == LEFT){
+                this.head[0]--;
+            } else if(this.dir == UP){
+                this.head[1]--;
+            } else if(this.dir == DOWN){
+                this.head[1]++;
+            }
+            while(this.tail.length >= this.length){
+                this.tail.shift();
+            }
+        }
+    }
+
+    collide(){
+        if(!this.dead){
+            if(this.head[0] == game.apple[0] && this.head[1] == game.apple[1]){
+                this.length += lengthIncrease;
+                game.spawnApple();
+            }
+            if(this.head[0] < 0 || this.head[0] > 63 || this.head[1] < 0 || this.head[1] > 47){
+                this.die();
+                return;
+            }
+            let parr = game.playerArr();
+            for(let j = 0; j < parr.length; j++){
+                let add = 0;
+                if(game.players[j].id == this.id){
+                    add = 1;
+                }
+                for(let i = 1 + add; i < parr[j].length; i++){
+                    if(this.head[0] == parr[j][i][0] && this.head[1] == parr[j][i][1]){
+                        this.die();
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    die(){
+        for(let i = 0; i < game.players.length; i++){
+            if(game.players[i].id == this.id){
+                game.players.splice(i, 1);
+            }
+        }
+        io.to(this.id).emit('death', {
+
+        });
+        this.dead = true;
+    }
 }
 
 class Game {
     constructor(){
-        this.players = {};
+        this.players = [];
         this.apple = [];
         this.spawnApple();
         this.gameloop = setInterval(function(g){
-            if(game != 0){
-                game.update();
+            if(game != null){
+                game.loop();
             }
         }, 1000 / 20);
     }
-    update(){
-        if(Object.keys(this.players).length == 0){
-            game = 0;
+    loop(){
+        if(this.players.length == 0){
             clearInterval(this.gameloop);
+            game = null;
         }
-        io.emit('update', {
-            "apple": this.apple,
-            "players": this.players
+        this.players.forEach(function(p){
+            p.move();
         });
-        Object.keys(dropped).forEach(function(key){
-            dropped[key]++;
-            if(dropped[key] > timeout){
-                if(game != 0){
-                    delete game.players[key];
-                    delete dropped[key];
+        this.players.forEach(function(p){
+            p.collide();
+        });
+
+        io.emit('tick', {
+            "apple": this.apple,
+            "players": this.playerArr()
+        });
+
+    }
+    spawnApple(){
+        let trypos = [Math.floor(Math.random() * 64), Math.floor(Math.random() * 48)];
+        while(this.illegalApple(trypos)){
+            trypos = [Math.floor(Math.random() * 64), Math.floor(Math.random() * 48)];
+        }
+        this.apple = trypos;
+    }
+    illegalApple(pos){
+        this.playerArr().forEach(function(p){
+            for(let i = 1; i < p.length; i++){
+                if(p[i][0] == pos[0] && p[i][1] == pos[1]){
+                    return true;
                 }
             }
         });
+        return false;
     }
-
-
-    spawnApple(){
-        this.apple = [Math.floor(Math.random() * 64), Math.floor(Math.random() * 48)];
+    playerArr(){
+        let retarr = [];
+        this.players.forEach(function(p){
+            let addarr = [];
+            addarr.push(p.data);
+            addarr.push(p.head);
+            p.tail.forEach(function(t){
+                addarr.push(t);
+            });
+            retarr.push(addarr);
+        });
+        return retarr;
+    }
+    safeSpawnPos(){
+        let failed = 8;
+        let attempt = [Math.floor(Math.random() * 56), Math.floor(Math.random() * 48)];
+        while(this.illegalAttempt(attempt, failed)){
+            failed--;
+            if(failed < 2){
+                attempt = [Math.floor(Math.random() * 56), Math.floor(Math.random() * 48)];
+                while(this.illegalApple(attempt)){
+                    attempt = [Math.floor(Math.random() * 56), Math.floor(Math.random() * 48)];
+                }
+                return attempt;
+            }
+        }
+        return attempt;
+    }
+    illegalAttempt(attempt, strict){
+        this.playerArr().forEach(function(p){
+            for(let i = 1; i < p.length; i++){
+                if((p[i][0] - attempt[0]) < strict && (p[i][0] - attempt[0]) > 0 && Math.abs(p[i][1] - attempt[1]) < strict){}
+                return true;
+            }
+        });
+        return false;
     }
 }
--- public/snake.js
+++ public/snake.js
@@ -1,145 +1,132 @@
-let socket = io();
-let data;
-let tick = 0;
-
-let player, cnv;
-let players = {};
-let apple = [-1, -1];
+let socket;
+let cnv, names, scores;
+let apple, players;
 const tileSize = 10;
+let gameState;
 
 function setup(){
-    noLoop();
     blendMode(REPLACE);
     cnv = createCanvas(640, 480);
     centerCanvas();
-    player = new Player();
+    names = document.getElementById("names").childNodes;
+    scores = document.getElementById("scores").childNodes;
+    socket = io();
+    socket.on('tick', function(data){
+        apple = data["apple"];
+        players = data["players"];
+        draw();
+    });
+    socket.on('death', function(data){
+       gameState = "menu";
+       document.getElementById("menu").style.opacity = "0.0";
+       document.getElementById("menu").style.display = 'inline';
+       for(let i = 0; i < 101; i++){
+           setTimeout(function(n){
+               document.getElementById("menu").style.opacity = "" + n/100;
+           }, 10 * i, i);
+       }
+    });
+    gameState = "menu";
+    noLoop();
+}
+
+function draw(){
+    background(0);
+    fill([255, 0, 0]);
+    rect(apple[0] * tileSize, apple[1] * tileSize, tileSize, tileSize);
+    let top5 = [];
+    players.forEach(function(player){
+        fill((player[0]["color"]));
+        for(let i = 1; i < player.length; i++){
+            rect(player[i][0] * tileSize, player[i][1] * tileSize, tileSize, tileSize);
+        }
+        if(top5.length == 0){
+            top5.push(player[0]);
+        }
+        else if(top5.length < 5){
+            for(let i = top5.length - 1; i >= 0; i--){
+                if(top5[i]["score"] > player[0]["score"]){
+                    top5.splice(i + 1, 0, player[0]);
+                }
+                else if(i == 0) {
+                    top5.splice(0, 0, player[0]);
+                }
+            }
+        }
+        else if(top5[4]["score"] < player[0]["score"]){
+            for(let i = 3; i >= 0; i--){
+                if(top5[i]["score"] > player[0]["score"]){
+                    top5.splice(i + 1, 0, player[0]);
+                    top5.pop();
+                }
+                else if(i == 0) {
+                    top5.splice(0, 0, player[0]);
+                    top5.pop();
+                }
+            }
+        }
+    });
+    for(let i = 0; i < 5; i++){
+        if(i >= top5.length){
+            scores[i * 2 + 1].innerHTML = "";
+            names[i * 2 + 1].innerHTML = "";
+        }
+        else {
+            scores[i * 2 + 1].innerHTML = top5[i]["score"];
+            if(top5[i]["name"].length > 9){
+                top5[i]["name"] = top5[i]["name"].substring(0, 10);
+            }
+            names[i * 2 + 1].innerHTML = top5[i]["name"];
+        }
+    }
+
+}
+
+function play(){
+    gameState = "ingame";
+    document.getElementById("menu").style.display = 'none';
+    socket.emit('newplayer', {
+        "name": document.getElementById("usernameinpt").value
+    });
 }
 
 function centerCanvas(){
     let x = (windowWidth - width) / 2;
     let y = 2 * (windowHeight - height) / 5;
     cnv.position(x, y);
+    document.getElementById("scoreboard").style.top = document.getElementById("defaultCanvas0").style.top;
+    document.getElementById("scoreboard").style.display = "inline-block";
+    document.getElementById("sourcecode").style.top = "calc(" + document.getElementById("defaultCanvas0").style.top + " + 300px)";
+    document.getElementById("sourcecode").style.display = "inline-block";
+
 }
 
 function windowResized(){
     centerCanvas();
 }
 
-function draw(){
-    background(0);
-    if(!player.dead){
-        updatePlayer();
-    }
-    let reply = [[player.pos[0], player.pos[1]]];
-    socket.emit('updatereply', {
-        "pieces": reply.concat(player.tail)
+function changeDir(dir){
+    socket.emit('changeDir', {
+        "dir": dir
     });
-    updateData();
-    player.collide();
-    drawPlayers();
-}
-
-socket.on('update', function(_data){
-    data = _data;
-    redraw();
-});
-
-socket.on('connectdata', function(_data){
-    player.color = _data["color"];
-    player.pos = _data["pos"];
-});
-
-function updatePlayer(){
-    player.update();
-}
-
-function updateData(){
-    apple = data["apple"];
-    players = data["players"];
-}
-
-function drawPlayers(){
-    fill([255, 0, 0]);
-    rect(apple[0] * tileSize, apple[1] * tileSize, tileSize, tileSize);
-    Object.keys(players).forEach(function(key){
-        fill(players[key].color);
-        players[key].pieces.forEach(function(piece){
-            rect(piece[0] * tileSize, piece[1] * tileSize, tileSize, tileSize);
-        });
-    })
 }
 
 const UP = 0, RIGHT = 1, DOWN = 2, LEFT = 3;
-const lengthIncrease = 3;
-let dir = UP;
 function keyPressed(){
-    if(keyCode == 87){
-        player.changeDir(UP);
-    } else if(keyCode == 68){
-        player.changeDir(RIGHT);
-    } else if(keyCode == 83){
-        player.changeDir(DOWN);
-    } else if(keyCode == 65){
-        player.changeDir(LEFT);
-    }
-}
-
-class Player {
-    constructor(){
-        this.pos = [0, 0];
-        this.tail = [];
-        this.length = 3;
-        this.dir = RIGHT;
-        this.nextDir = RIGHT;
-        this.color = [0, 0, 0];
-        this.dead = false;
-    }
-    update(){
-        this.tail.push([this.pos[0], this.pos[1]]);
-        this.move();
-        while(this.tail.length >= this.length){
-            this.tail.shift();
+    if(gameState == "menu"){
+        if(keyCode == 13){
+            play();
         }
     }
-    changeDir(_dir){
-        if(this.dir != _dir - 2 && this.dir != _dir + 2){
-            this.nextDir = _dir;
+    else if(gameState == "ingame"){
+        if(keyCode == 87){
+            changeDir(UP);
+        } else if(keyCode == 68){
+            changeDir(RIGHT);
+        } else if(keyCode == 83){
+            changeDir(DOWN);
+        } else if(keyCode == 65){
+            changeDir(LEFT);
         }
-
-    }
-    collide(){
-        if(apple[0] == this.pos[0] && apple[1] == this.pos[1]){
-            this.length += lengthIncrease;
-            socket.emit('spawnapple', 0);
-        }
-        if(this.pos[0] < 0 || this.pos[0] > 63 || this.pos[1] < 0 || this.pos[1] > 47 ){
-            this.die();
-        }
-        Object.keys(players).forEach(function(key){
-            players[key].pieces.forEach(function(piece){
-                if(player.pos[0] == piece[0] && player.pos[1] == piece[1]){
-                    player.die();
-                }
-            })
-        })
-    }
-    move(){
-        this.dir = this.nextDir;
-        if(this.dir == RIGHT){
-            this.pos[0]++;
-        } else if(this.dir == LEFT){
-            this.pos[0]--;
-        } else if(this.dir == UP){
-            this.pos[1]--;
-        } else if(this.dir == DOWN){
-            this.pos[1]++;
-        }
-    }
-
-    die(){
-        this.dead = true;
-        setTimeout(function(){
-            socket.emit('die', 0);
-        })
     }
 }
